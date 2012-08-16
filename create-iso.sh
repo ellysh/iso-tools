8bdffd700b8d44b8aae1015e75b4a5b570d95170
Ilya Shpigor <petrsum@gmail.com>
2012-08-16 10:45:58 +0400

    Add the iso file creation script

---
+++ create-iso.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Script for iso image creating
+# Usage: create-iso.sh <iso-filename> <source-files-path>
+
+ISO_NAME="$1"
+SOURCE_PATH="$2"
+
+mkisofs -o "$ISO_NAME" -b "$SOURCE_PATH/isolinux/isolinux.bin" -c boot.cat -no-emul-boot \
+        -boot-load-size 4 -boot-info-table -J -hide-rr-moved -R "$SOURCE_PATH"
