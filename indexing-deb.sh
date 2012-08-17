bb367ad9fe4bc4984f63a039fb3c621fea0923d9
Ilya Shpigor <petrsum@gmail.com>
2012-08-17 10:04:13 +0400

    Add the deb packages indexing script

---
+++ indexing-deb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script for indexing deb packages
+
+. src/functions.sh
+
+print_help "Usage: indexing-deb.sh <deb-packages-path>" "$1"
+
+DEB_PATH="$1"
+
+dpkg-scanpackages -m "$DEB_PATH" /dev/null | gzip -9c > "$DEB_PATH"/Packages.gz
