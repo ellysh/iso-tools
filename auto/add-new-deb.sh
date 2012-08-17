f145a6528bbe94c94f62bcd0a08b08502d10f589
Ilya Shpigor <petrsum@gmail.com>
2012-08-17 16:30:26 +0400

    Minor fixes in the add-new-deb.sh script

--- auto/add-new-deb.sh
+++ auto/add-new-deb.sh
@@ -11,12 +11,12 @@ ISO_FILE="/home/elly/Projects/ubuntu.iso"
 
 cd_safe ".."
 
-deb-to-seed.sh "$SEED_FILE" "$DEB_PATH"
+./deb-to-seed.sh "$SEED_FILE" "$DEB_PATH"
 
-indexing-deb.sh "$DEB_PATH"
+./indexing-deb.sh "$DEB_PATH"
 
-md5sum-calc.sh "$SOURCE_PATH"
+./md5sum-calc.sh "$SOURCE_PATH"
 
-create-iso.sh "$ISO_FILE" "$SOURCE_PATH"
+./create-iso.sh "$ISO_FILE" "$SOURCE_PATH"
 
 restore_directory
